44f04e5f1fc1dde602fc015776c945add4de02ab
Rodrigo Soberon <rodrigo.soberon.a@gmail.com>
2022-07-27 12:13:49 -0300

    cart reducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,12 +1,36 @@
 import CartContext from "./cart-context";
+import { useReducer } from "react";
+
+const defaultCartState = {
+  item: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updateItems = state.items.concat(action.item);
+    const updateTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return { item: updateItems, totalAmount: updateTotalAmount };
+  }
+  return defaultCartState;
+};
 
 const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
-  const removeItemToCartHandler = (item) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+  const removeItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
 
   const cartContex = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemToCartHandler,
   };
